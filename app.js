01065ca82c1fbee4b4ba1e766d60f3ed36c77184
PEDRO VICTOR RODRIGUES DE ALMEIDA <24122478@CFP127ALUNO.LOCAL>
2025-02-11 17:04:58 -0300

    configurando cards

--- app.js
+++ app.js
@@ -6,14 +6,7 @@ const cidades = [
     {nome: 'Barueri', cor : 'blue', icon : 'download.png', },
     {nome: 'Osasco', cor : 'white', icon : 'download.png'},
 ]
-const imagens = [
-    {nome : 'Memphis', imagem: ''}
-    {nome : 'Yuri', imagem: ''},
-    {nome : 'Garro', imagem: ''},
-    {nome : 'Bidu', imagem: ''},
-    {nome : 'Talles', imagem: ''}
-    
-]
+
 
 
 function criarMenu(cidades) {
@@ -35,10 +28,59 @@ novoItem.appendChild(novoLink)
 novoItem.style = `--cor-hover: ${cidades.cor}`
 
 menu.appendChild(novoItem)
+
 }
 
-function criarCard(card){
-    const
+const produtos = [
+    { 
+        nome: 'Corinthians', 
+        img: 'corinthians-.png', 
+        preco: 'R$499,99', 
+        cor: 'blueviolet', 
+        estrelas: 5,
+        descricao: 'Sport Clube Corinthians Paulista"'
+    },
+    { 
+        nome: 'Palmeiras', 
+        img: 'porcada.png', 
+        preco: 'R$00,00', 
+        cor: 'blueviolet', 
+        estrelas: 0,
+        descricao: 'Sem Mundial'
+    },
+    { 
+        nome: 'Santos', 
+        img: 'santos.png', 
+        preco: 'R$60,00', 
+        cor: 'blueviolet', 
+        estrelas: 4,
+        descricao: 'Time da Baixada Santista'
+    },
+    { 
+        nome: 'São Paulo', 
+        img: 'sp-time ruim.png', 
+        preco: 'R$24,00', 
+        cor: 'blueviolet', 
+        estrelas: 5,
+        descricao: 'Iphone 16 Pro Max'
+    }
+]
+function criarCard(produto) {
+    const container = document.getElementById('container')
+    const card = document.createElement('div')
+    card.classList.add('Card')
+
+    card.innerHTML = `
+        <img src="./img/${produto.img}" alt="${produto.nome}">
+        <h3>${produto.nome}</h3>
+        <p>${produto.descricao}</p>
+        <p class="price">${produto.preco}</p>
+        <div class="stars">${'★'.repeat(produto.estrelas)}${'☆'.repeat(5 - produto.estrelas)}</div>
+        <button class="${produto.cor}">Comprar Agora</button>
+    `
+
+    container.appendChild(card)
 }
+
 
 cidades.forEach(criarMenu)
